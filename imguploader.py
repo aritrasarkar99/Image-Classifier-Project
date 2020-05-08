37c8643f23fa2dfc4e4a74cf918466a593362d15
aritrasarkar99 <aritra.sarkar1999@gmail.com>
2020-05-08 14:10:31 +0530

    first commit

---
+++ imguploader.py
@@ -0,0 +1,27 @@
+from flask import Flask,redirect,request,url_for
+from flask import render_template as ren
+import os
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return ren("index.html")
+
+@app.route("/img-upload", methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+
+        if request.files:
+            image = request.files['image']
+            if secure_filename(image.filename):
+                filename = image.filename
+                ext  = filename.rsplit(".",1)[1]
+                ext = ext.lower()
+                p = "templates\\uploads\\"+"uploaded_img"+"."+ext
+                image.save(p)
+                return redirect(request.url)
+
+    return ren("index.html")
+if __name__ == '__main__':
+     app.run(debug=True)
