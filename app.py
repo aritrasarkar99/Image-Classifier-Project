1728bc43aa3ad654cfe9af3244b85c2109ca38dc
aritrasarkar99 <aritra.sarkar1999@gmail.com>
2020-05-09 16:11:27 +0530

    commit

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import render_template as ren
 import os
 from werkzeug.utils import secure_filename
 import uuid
+
 app = Flask(__name__)
 
-# FILE_PATH = os.environ.get("FILE_PATH")
+UPLOAD_PATH = 'uploads'
+app.config['UPLOAD_PATH'] = UPLOAD_PATH
 
 @app.route("/")
 def home():
@@ -13,11 +15,13 @@ def home():
 
 @app.route("/img-upload", methods=['GET','POST'])
 def upload():
+    
     if request.method == 'POST':
 
         if request.files:
 
             image = request.files['image']
+            print(image)
             id = uuid.uuid1()
 
             if secure_filename(image.filename):
@@ -32,6 +36,43 @@ def upload():
 
     return ren("index.html")
 
+@app.route('/upload-new',methods=['GET','POST'])
+def upload_file():
+     if request.method == 'POST':
+        
+        if request.files:
+
+            image = request.files['image']
+            id = uuid.uuid1()
+
+            if image:
+
+                filename = image.filename
+                # ext =  filename.rsplit(".",1)[1]
+                # filename = id.hex + "." + ext  ######### FileName of uploaded file ############
+                filename = secure_filename(image.filename)
+
+                file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+                image.save(file_path)
+                return send_from_directory(app.config['UPLOAD_PATH'],
+                               filename)
+
+     return ren("index.html")
+
+from flask import send_from_directory
+from werkzeug.middleware.shared_data import SharedDataMiddleware
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'],
+                               filename)
+
+
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_PATH']
+})
 
 if __name__ == '__main__':
      app.run(debug=True)
